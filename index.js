2eae3328c0be29e645831c12282fe2b84a8efa86
Daniel Caicedo <kpeeer@hotmail.com>
2022-03-11 01:01:07 -0500

    version 2 body

--- index.js
+++ index.js
@@ -8,3 +8,48 @@ function clickButton(){
 
 };
 
+function product(){
+    let navigation = document.querySelector(".container");
+    navigation.classList.toggle("noView");
+   };
+   
+function product1(){
+    let navigation = document.querySelector(".container");
+    navigation.classList.toggle("noView1");
+   };
+
+/* function product(){
+    let navigation = document.querySelector(".container");
+    navigation.classList.toggle("noView2");
+   };
+
+function product(){
+    let navigation = document.querySelector(".container");
+    navigation.classList.toggle("noView3");
+   }; */
+
+
+
+
+
+
+
+
+/* let priceRandom = document.querySelectorAll(".priceRandom")
+for(price of priceRandom){
+    let num = Math.round(Math.random()*10 + 300);
+    document.getElementById("price").innerHTML = `$ ${num}`; 
+}
+ */
+
+
+    
+
+ /* let navigation = document.querySelector(".nav-link");
+    let text = navigation.value;
+    console.log(navigation)
+    let images  =[];
+
+    for(i=0; i<8; i++){
+        images[i] = `<img title="${text}" src="imagenes/${text}.png">`
+    }; */
